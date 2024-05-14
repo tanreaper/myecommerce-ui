bbb6b77d84e55e8202d28405e78a96ad24d0233e
tanreaper <paulsapto@gmail.com>
2024-05-14 15:54:16 -0700

    + added front end post logic

--- src/login.js
+++ src/login.js
@@ -1,76 +1,94 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Login = (props) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailError, setEmailError] = useState('')
-  const [passwordError, setPasswordError] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [emailError, setEmailError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
 
-  const navigate = useNavigate()
 
-  const onButtonClick = () => {
+    const navigate = useNavigate()
+
     const onButtonClick = () => {
         // Set initial error values to empty
         setEmailError('')
         setPasswordError('')
-      
+        
         // Check if the user has entered both fields correctly
         if ('' === email) {
-          setEmailError('Please enter your email')
-          return
+            setEmailError('Please enter your email')
+            return
         }
-      
-        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-          setEmailError('Please enter a valid email')
-          return
-        }
-      
-        if ('' === password) {
-          setPasswordError('Please enter a password')
-          return
-        }
-      
-        if (password.length < 7) {
-          setPasswordError('The password must be 8 characters or longer')
-          return
-        }
-      
-        // Authentication calls will be made here...
-      }
-  }
 
-  return (
-    <div className={'mainContainer'}>
-      <div className={'titleContainer'}>
-        <div>Login</div>
-      </div>
-      <br />
-      <div className={'inputContainer'}>
-        <input
-          value={email}
-          placeholder="Enter your email here"
-          onChange={(ev) => setEmail(ev.target.value)}
-          className={'inputBox'}
-        />
-        <label className="errorLabel">{emailError}</label>
-      </div>
-      <br />
-      <div className={'inputContainer'}>
-        <input
-          value={password}
-          placeholder="Enter your password here"
-          onChange={(ev) => setPassword(ev.target.value)}
-          className={'inputBox'}
-        />
-        <label className="errorLabel">{passwordError}</label>
-      </div>
-      <br />
-      <div className={'inputContainer'}>
-        <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
-      </div>
-    </div>
-  )
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+            setEmailError('Please enter a valid email')
+            return
+        }
+
+        if ('' === password) {
+            setPasswordError('Please enter a password')
+            return
+        }
+
+        if (password.length < 7) {
+            setPasswordError('The password must be 8 characters or longer')
+            return
+        }
+        console.log(email)
+        console.log(password)
+
+        // axios.get(`http://localhost:5001/login`).then(res => {
+        //     console.log(res)
+        //   })
+
+        axios({
+            method: 'POST',
+            url: 'http://localhost:5001/login',
+            withCredentials: false,
+            data: {
+                email,
+                password
+            },
+        }).then(res => {
+            console.log(res)
+        })
+
+        // Authentication calls will be made here...
+    }
+
+    return (
+        <div className={'mainContainer'}>
+            <div className={'titleContainer'}>
+                <div>Login</div>
+            </div>
+            <br />
+            <div className={'inputContainer'}>
+                <input
+                    value={email}
+                    placeholder="Enter your email here"
+                    onChange={(ev) => setEmail(ev.target.value)}
+                    className={'inputBox'}
+                />
+                <label className="errorLabel">{emailError}</label>
+            </div>
+            <br />
+            <div className={'inputContainer'}>
+                <input
+                    value={password}
+                    placeholder="Enter your password here"
+                    onChange={(ev) => setPassword(ev.target.value)}
+                    className={'inputBox'}
+                />
+                <label className="errorLabel">{passwordError}</label>
+            </div>
+            <br />
+            <div className={'inputContainer'}>
+                <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
+            </div>
+        </div>
+    )
 }
 
 export default Login
